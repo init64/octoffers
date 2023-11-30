acf4fde000e28bd534f92d032d2d7270b5d9c485
Viktor Andriichuk <viktorandriichuk@192-168-0-218.rdsnet.ro>
2023-11-30 14:00:44 +0200

    Rewrite comments

--- octoffers/main.py
+++ octoffers/main.py
@@ -1,18 +1,27 @@
+
+
 from fire import Fire
 from platforms.djinni import Djinni
 
 
 class Octoffers:
     def __init__(self):
-        self.coverletter = "Hello, I'm looking for job"
+        self.djinni_driver = Djinni()
+
+    def fetch_all_jobs_by_role(self, role="Data+Engineer", tools=None, min_salary=None, exclusion_words=['Junior', 'platform', 'Hydrographic']):
+        url = f"https://{self.djinni_driver.driver_instance.domain}/jobs/"
+        self.djinni_driver.fetch_jobs(url, role, tools, min_salary, exclusion_words)
+
+    def fetch_all_jobs_for_me(self, tools=['dbt', 'aws', 'python', 'pandas'], min_salary=None, exclusion_words=['Junior', 'platform', 'Hydrographic']):
+        url = f"https://{self.djinni_driver.driver_instance.domain}/jobs/my/"
+        self.djinni_driver.fetch_jobs(url, tools, min_salary, exclusion_words)
+
+    def apply_jobs(self):
+        self.djinni_driver.apply_jobs()
+
+    def manual_authorization(self):
+        self.djinni_driver.manual_authorization()
 
 
-    def djinni(self, apply: bool = False):
-        driver = Djinni()
-        return driver.apply(self.coverletter) if apply else "driver's under development"
-
-    
-
-
-
-Fire(Octoffers)
+if __name__ == "__main__":
+    Fire(Octoffers)
---
+++ octoffers/platforms/driver.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+load_dotenv()
+
+# Getting cookie values
+session_id = os.getenv("DJINNI_SESSION_ID")
+hj_session_user = os.getenv("DJINNI_HJ_SESSION_USER")
+csrf_token = os.getenv("DJINNI_CSRF_TOKEN")
+
+# Generating a list of cookies
+session_cookies = [
+    {'name': 'sessionid', 'value': session_id, 'domain': '.djinni.co'},
+    {'name': '_hjSessionUser_3704742', 'value': hj_session_user, 'domain': '.djinni.co'},
+    {'name': 'csrftoken', 'value': csrf_token, 'domain': '.djinni.co'}
+]
+
+
+class Driver:
+    def __init__(self, domain):
+        self.domain = domain
+        self.session_cookies = session_cookies
+
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.session_authorization()
+
+    def session_authorization(self):
+        self.driver.get(f"https://{self.domain}")
+
+        # Adding an explicit wait to ensure the page has loaded
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "body"))
+        )
+
+        for cookie in self.session_cookies:
+            self.driver.add_cookie(cookie)
+
+        # Reloading the page to apply cookies
+        self.driver.get(f"https://{self.domain}")
