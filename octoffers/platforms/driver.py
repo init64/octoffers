468e8625a431e1b5661548fd1e1d27a7849103b3
webgtx <alx.zolotarov@gmail.com>
2024-01-10 22:39:58 +0000

    Update: small base driver refactoring

--- octoffers/platforms/driver.py
+++ octoffers/platforms/driver.py
@@ -1,24 +1,25 @@
-import os
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from os import getenv, environ
+from os.path import isfile
 
 load_dotenv()
 
 # Getting cookie values
-session_id = os.getenv("DJINNI_SESSION_ID")
+session_id = getenv("DJINNI_SESSION_ID")
 
 # Generating a list of cookies
 session_cookies = [{"name": "sessionid", "value": session_id, "domain": ".djinni.co"}]
-
 
 class Driver:
     def __init__(self, domain):
         self.domain = domain
         self.session_cookies = session_cookies
+        self.octoffers_path = f"{environ['HOME']}/.config/octoffers"
 
     def _initiate_driver(self, *argv):
         chrome_options = Options()
@@ -27,6 +28,7 @@ class Driver:
         self.driver = webdriver.Chrome(options=chrome_options)
         # self.driver.implicitly_wait(5)
         self.wait = WebDriverWait(self.driver, 5)
+
 
     def session_authorization(self):
         self.driver.get(f"https://{self.domain}")
@@ -40,4 +42,4 @@ class Driver:
             self.driver.add_cookie(cookie)
 
         # Reloading the page to apply cookies
-        self.driver.get(f"https://{self.domain}")
+        self.driver.refresh()
